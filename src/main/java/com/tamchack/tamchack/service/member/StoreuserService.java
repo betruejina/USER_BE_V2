7cb62872208e75dc9ce4083514b404ee36b1a009
JINA <63455858+betruejina@users.noreply.github.com>
2021-04-07 13:12:31 +0900

    Delete StoreuserService.java

--- src/main/java/com/tamchack/tamchack/service/member/StoreuserService.java
+++
@@ -1,8 +0,0 @@
-package com.tamchack.tamchack.service.member;
-
-import com.tamchack.tamchack.payload.request.StoreuserSignUpRequest;
-
-public interface StoreuserService {
-    void storeuserSignUp(StoreuserSignUpRequest storeuserSignUpRequest);
-
-}

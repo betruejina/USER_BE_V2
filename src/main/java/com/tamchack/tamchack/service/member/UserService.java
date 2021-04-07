864ca77804d1d293f7afe6fa980430deebfe6d66
JINA <63455858+betruejina@users.noreply.github.com>
2021-04-07 13:12:20 +0900

    Delete UserService.java

--- src/main/java/com/tamchack/tamchack/service/member/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.tamchack.tamchack.service.member;
-
-import com.tamchack.tamchack.payload.request.UserSignUpRequest;
-
-public interface UserService {
-    void userSignUp(UserSignUpRequest userSignUpRequest);
-}

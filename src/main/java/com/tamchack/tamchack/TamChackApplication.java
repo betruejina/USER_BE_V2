86540ead4c94abed77030d093a00bf7f10256f4e
yujina <jina6962@gmail.com>
2021-04-21 23:00:13 +0900

    [UPDATE]

--- src/main/java/com/tamchack/tamchack/TamChackApplication.java
+++ src/main/java/com/tamchack/tamchack/TamChackApplication.java
@@ -7,8 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TamChackApplication {
 
 	public static void main(String[] args) {
-		System.out.printf("유진아 바보");
+		System.out.printf("TEST");
 		SpringApplication.run(TamChackApplication.class, args);
 	}
-
 }

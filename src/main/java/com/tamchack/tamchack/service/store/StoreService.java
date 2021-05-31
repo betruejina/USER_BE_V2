d524495723dd3273db12fd141c61c0125b80be10
yujina <jina6962@gmail.com>
2021-05-31 23:42:08 +0900

    [UPDATE]

--- src/main/java/com/tamchack/tamchack/service/store/StoreService.java
+++ src/main/java/com/tamchack/tamchack/service/store/StoreService.java
@@ -1,11 +1,14 @@
 package com.tamchack.tamchack.service.store;
 
-import com.tamchack.tamchack.payload.response.ApplicationListResponse;
+import com.tamchack.tamchack.dto.request.member.RevisePasswordRequest;
+import com.tamchack.tamchack.dto.request.store.ReviseStoreRequest;
+import com.tamchack.tamchack.dto.response.address.ApplicationListResponse;
 import org.springframework.data.domain.Pageable;
 
 public interface StoreService {
 
-    // void updateStoreInfo(ReviseStoreRequest reviseStoreRequest, String token);
+    void updateStoreInfo(ReviseStoreRequest reviseStoreRequest, String token);
 
     ApplicationListResponse searchStore(String query, Pageable page);
+
 }

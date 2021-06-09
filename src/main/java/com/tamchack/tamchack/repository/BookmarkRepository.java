22f31f7d6cd2f3fe1ed05cdb5d6d1e5745a2b431
yujina <jina6962@gmail.com>
2021-06-09 23:31:31 +0900

    [FIX]

--- src/main/java/com/tamchack/tamchack/repository/BookmarkRepository.java
+++ src/main/java/com/tamchack/tamchack/repository/BookmarkRepository.java
@@ -1,16 +1,17 @@
 package com.tamchack.tamchack.repository;
 
 import com.tamchack.tamchack.domain.store.Bookmark;
+import com.tamchack.tamchack.domain.store.BookmarkKey;
 import com.tamchack.tamchack.domain.store.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface BookmarkRepository extends JpaRepository<Bookmark, String> {
+public interface BookmarkRepository extends JpaRepository<Bookmark, BookmarkKey> {
 
-    boolean existsByStoreIdAndUserId(Store storeId, String userId);
+    boolean existsByStoreAndUserId(Store storeId, String userId);
 
-    void deleteByStoreIdAndUserId(Store storeId, String userId);
+    void deleteByStoreAndUserId(Store storeId, String userId);
 
     List<Bookmark> findAllByUserId(String userId);
 
--- src/main/java/com/tamchack/tamchack/repository/StockRepository.java
+++ src/main/java/com/tamchack/tamchack/repository/StockRepository.java
@@ -1,2 +1,16 @@
-package com.tamchack.tamchack.repository;public interface StockRepository {
+package com.tamchack.tamchack.repository;
+
+import com.tamchack.tamchack.domain.book.Book;
+import com.tamchack.tamchack.domain.book.Stock;
+import com.tamchack.tamchack.domain.book.StockKey;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StockRepository extends JpaRepository<Stock, StockKey> {
+
+    boolean existsByStoreIdAndBook(Integer storeId, Book book);
+
+    void deleteByStoreIdAndBook(Integer storeId, Book book);
+
 }
